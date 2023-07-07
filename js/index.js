ddd225e12392f220b71255c8f4e938645d8b2b48
Ivan Fernandez <nahuel.nf13@gmail.com>
2023-07-07 16:10:42 -0300

    Index casi terminado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// CLASE 1
 // let nombre = "ivan"
 // let apellido = "fernandez"
 
@@ -8,23 +9,46 @@
 // console.log (alert)
 
 
-// De 3 numeros que le doy, me imprime el mas grande
-let t
-let mayor
-let numero1 = prompt ("ingrese un numero")
-let numero2 = prompt ("ingrese otro numero")
-let numero3 = prompt ("ingrese un ultimo numero")
+// //CLASE 2
+// //De 3 numeros que le doy, me imprime el mas grande
+//let t
+//let mayor
+//let numero1 = prompt ("ingrese un numero")
+//let numero2 = prompt ("ingrese otro numero")
+//let numero3 = prompt ("ingrese un ultimo numero")
 
-if (numero1 > numero2) {
-    t = numero1
-} else {
-    t = numero2
-}
+//if (numero1 > numero2) {
+//   t = numero1
+//} else {
+//   t = numero2
+//}
 
-if (t > numero3) {
-    mayor = t
-} else {
-    mayor = numero3
-}
+//if (t > numero3) {
+//   mayor = t
+//} else {
+//   mayor = numero3
+//}
 
-console.log ("el Numero mayor es " + mayor)
+//console.log ("el Numero mayor es " + mayor)
+
+// //CLASE 3
+//for (let i = 0; i < 10; i++) {
+//    console.log (i)
+//}
+
+//let nombre = "ivan"
+//console.log(nombre.length)
+
+//for (let i = 0; i < nombre.length; i++) {
+//    console.log (i)
+//}
+
+// //TABLA DE MULTIPLICAR
+//let ingresarNumero = parseInt(prompt("ingresar numero"))
+
+//for (let i = 1; i <= 10; i++) {
+//    let resultado = ingresarNumero * i
+//    console.log(ingresarNumero + " x " + i + " = " + resultado)
+//}
+
+
