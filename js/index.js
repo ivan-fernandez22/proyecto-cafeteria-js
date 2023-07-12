cd244bf04b97c00e23ab9940cbb1a8f8d62a9eb5
Ivan Fernandez <nahuel.nf13@gmail.com>
2023-07-12 00:15:27 -0300

    se terminó la primer pre entrega de js y se modificó un poco el footer

--- js/index.js
+++ js/index.js
@@ -1,54 +1,52 @@
-// CLASE 1
-// let nombre = "ivan"
-// let apellido = "fernandez"
+// Condicional que me pide ser mayor para acceder al sitio
 
-// let usuario = prompt ("ingrese un nombre de usuario")
-// console.log (usuario)
+let edad = prompt("Ingrese su edad")
+let mayorEdad = "Usted es mayor de edad, puede acceder al sitio"
+let menorEdad = "Usted es menor de edad, no puede acceder al sitio"
+let noIngresoNada = "Usted no ingresó una edad"
 
-// alert ("Su nombre de usuario " + usuario + "se ha guardado con exito!")
-// console.log (alert)
-
-
-// //CLASE 2
-// //De 3 numeros que le doy, me imprime el mas grande
-//let t
-//let mayor
-//let numero1 = prompt ("ingrese un numero")
-//let numero2 = prompt ("ingrese otro numero")
-//let numero3 = prompt ("ingrese un ultimo numero")
-
-//if (numero1 > numero2) {
-//   t = numero1
-//} else {
-//   t = numero2
-//}
-
-//if (t > numero3) {
-//   mayor = t
-//} else {
-//   mayor = numero3
-//}
-
-//console.log ("el Numero mayor es " + mayor)
-
-// //CLASE 3
-//for (let i = 0; i < 10; i++) {
-//    console.log (i)
-//}
-
-//let nombre = "ivan"
-//console.log(nombre.length)
-
-//for (let i = 0; i < nombre.length; i++) {
-//    console.log (i)
-//}
-
-// //TABLA DE MULTIPLICAR
-//let ingresarNumero = parseInt(prompt("ingresar numero"))
-
-//for (let i = 1; i <= 10; i++) {
-//    let resultado = ingresarNumero * i
-//    console.log(ingresarNumero + " x " + i + " = " + resultado)
-//}
+if (edad < 18) {
+    console.log (menorEdad)
+} else if (edad >= 18) {
+    console.log (mayorEdad)
+} else if (edad != isNaN) {
+    console.log (noIngresoNada)
+}
 
 
+
+// Simulador interactivo que dependiendo de mi zona de residencia, me dice 
+// cuanto es el total de lo que compramos + el envio aproximadamente
+
+let precioProducto = Number (prompt ("Ingrese el precio total que figura en el carrito"))
+let envioProducto = prompt ("Ingrese la zona en donde vive, sus opciones son: zona sur, zona oeste, zona norte o capital")
+
+const envioZonaSur = Number (500)
+const envioCapital = Number (1000)
+const envioZonaOeste = Number (1000)
+const envioZonaNorte = Number (1500)
+
+function precioEnvio () {
+    while (envioProducto != "") {
+        switch (envioProducto) {
+            case "zona sur":
+                alert ("El costo final (envio + producto) es: " + "$" + (precioProducto + envioZonaSur));
+                break;
+            case "zona oeste":
+                alert ("El costo final (envio + producto) es: " + "$" + (precioProducto + envioZonaOeste));
+                break;
+            case "zona norte":
+                alert ("El costo final (envio + producto) es: " + "$" + (precioProducto + envioZonaNorte));
+                break;
+            case "capital":
+                alert ("El costo final (envio + producto) es: " + "$" + (precioProducto + envioCapital));
+                break;
+            default:
+                alert ("Usted no ingresó una zona valida")
+                break;
+        }
+        break
+    }
+}
+
+precioEnvio ()
