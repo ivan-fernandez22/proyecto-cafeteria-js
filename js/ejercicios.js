151f8b9cd5e42951f2d79c6cbe3f4cd255f59709
Ivan Fernandez <nahuel.nf13@gmail.com>
2023-07-24 23:27:53 -0300

    Se declaro los arrays y objetos para la segunda entrega

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -26,3 +26,66 @@
 // }
 
 // longitudPalabra ()
+
+
+
+
+
+// EJERCICIOS CLASE 08:
+
+// Generar un número aleatorio entre 1 y 100.
+console.log (Math.random () * 100 + 1)
+
+// Redondear el número 4.89 al entero más cercano.
+console.log (Math.round (4.89))
+
+// Calcular la raíz cuadrada de 25.
+console.log (Math.sqrt (25))
+
+// Obtener el valor absoluto de -15.
+console.log (Math.abs (-15))
+
+// Obtener el valor máximo entre 4, 18 y 10.
+console.log (Math.max (4, 18, 10))
+
+// Obtener el valor mínimo entre 5, 13 y 7.
+console.log (Math.min (5, 13, 7))
+
+// Generar un número decimal aleatorio entre 0 y 1.
+console.log (Math.random ())
+
+// Redondear hacia abajo el número 6.7.
+console.log (Math.floor (6.7))
+
+// Redondear hacia arriba el número 3.2.
+console.log (Math.ceil (3.2))
+
+// Obtener la fecha actual.
+console.log (new Date ())
+
+// Obtener el día del mes actual.
+
+// Obtener el mes actual (como número, donde enero es 0 y diciembre es 11).
+
+// Obtener el año actual.
+
+// Obtener la hora actual.
+
+// Obtener los minutos actuales.
+
+// Obtener el último día del mes actual.
+
+// Obtener el número de días que faltan para Navidad (25 de diciembre).
+
+// Obtener la fecha de mañana.
+
+// Obtener el nombre del día de la semana actual.
+
+// Obtener el número de segundos que han pasado desde el inicio del año.
+
+// Crea una función llamada generadorNumero que no tome ningún parámetro. Esta función debe devolver un número entero aleatorio entre 0 y 100.
+
+// Crea una función llamada variosNumeros que tome dos parámetros: generador y cantidad. La función variosNumeros debe mostrar en la consola cantidad números generados utilizando la función generador.
+
+// Ejecuta la función variosNumeros con el generador generadorNumero y la cantidad de X números.
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// // IDEA GENERAL DEL PROYECTO: 
+// // Agregarle a mi pagina web (cafetería) funcionalidad: Que tenga carrito de compras, simulador de envios, simulador de reservas, etc.
+
+
+// // PRIMERA ENTREGA:
+
 // // Simulador interactivo que dependiendo de mi zona de residencia, me dice 
 // // cuanto es el total de lo que compramos + el envio aproximadamente
 
@@ -34,45 +40,137 @@
 // precioEnvio ()
 
 
-// // Introduciendo arrays
-// class Cakes {
-//     constructor (nombre, precio, stock) {
-//         this.nombre = nombre.toUpperCase () //Convierte todo a mayuscula
-//         this.precio = parseFloat (precio) //Parseo el precio (me da con coma)
-//         this.stock = stock
-//     }
-//     obtenerPrecio () {
-//         console.log ("El precio del producto: " + this.nombre + " es: " + this.precio + "$ ARS")
-//     }
-//     obtenerStock () {
-//         if (this.stock === true) {
-//             console.log ("Hay stock de: " + this.nombre)
-//         } else {
-//             console.log ("No hay stock de: " + this.nombre)
-//         }
-//     }
-// } 
 
-// const arrayDeCakes = []
 
-// arrayDeCakes.push (
-//     new Cakes ("Chessecake de frutos rojos", 1000, true)
-// )
+// // SEGUNDA ENTREGA:
 
-// arrayDeCakes.push (
-//     new Cakes ("Chessecake de maracuya", 1000, true)
-// )
 
-// arrayDeCakes.push (
-//     new Cakes ("Torta de chocolate", 900, false)
-// )
+// // INSERTANDO OBJETOS Y ARRAYS: Guardando productos para un futuro carrito
 
-// console.log (arrayDeCakes)
+//  Seccion "Cakes|porciones" de la page Tienda de productos 
 
-// for (const prod of arrayDeCakes) {
-//     prod.obtenerPrecio ()
-// }
+class Cakes {
+    constructor (nombre, precio, stock) {
+        this.nombre = nombre.toUpperCase () //Convierte todo a mayuscula
+        this.precio = parseFloat (precio) //Parseo el precio (me da con coma)
+        this.stock = stock
+    }
+    obtenerPrecio () {
+        console.log ("El precio del producto: " + this.nombre + " es: " + this.precio + "$ ARS")
+    }
+    obtenerStock () {
+        if (this.stock === true) {
+            console.log ("Hay stock de: " + this.nombre)
+        } else {
+            console.log ("No hay stock de: " + this.nombre)
+        }
+    }
+} 
 
-// for (const prod of arrayDeCakes) {
-//     prod.obtenerStock ()
-// }
+const arrayDeCakes = []
+
+arrayDeCakes.push (new Cakes ("Porción de chessecake de frutos rojos", 1000, true))
+
+arrayDeCakes.push (new Cakes ("Porción de chessecake de maracuya", 1000, true))
+
+arrayDeCakes.push (new Cakes ("Porción de torta de chocolate", 900, false))
+
+arrayDeCakes.push (new Cakes ("Tarta individual de lemon pie", 950, true))
+
+arrayDeCakes.push (new Cakes ("Porción de torta oreo", 1100, true))
+
+arrayDeCakes.push (new Cakes ("Porción de chocotorta", 1100, true))
+
+arrayDeCakes.push (new Cakes ("Porción de carrot cake", 950, false))
+
+console.log (arrayDeCakes)
+
+
+//  Seccion "Sandwichs y salados" de la page Tienda de productos 
+
+class SandwichsYSalados {
+    constructor (nombre, precio, stock) {
+        this.nombre = nombre.toUpperCase ()
+        this.precio = parseFloat (precio)
+        this.stock = stock
+    }
+    obtenerPrecio () {
+        console.log ("El precio del producto: " + this.nombre + " es: " + this.precio + "$ ARS")
+    }
+    obtenerStock () {
+        if (this.stock === true) {
+            console.log ("Hay stock de: " + this.nombre)
+        } else {
+            console.log ("No hay stock de: " + this.nombre)
+        }
+    }
+} 
+
+const arrayDeSandwichsYSalados = [] 
+
+arrayDeSandwichsYSalados.push (new SandwichsYSalados("Tostados de jamon y queso", 1300, true))
+
+arrayDeSandwichsYSalados.push (new SandwichsYSalados("Tostadas Le Paradis", 1450, true))
+
+arrayDeSandwichsYSalados.push (new SandwichsYSalados("Sandwich mediterraneo", 1700, false))
+
+arrayDeSandwichsYSalados.push (new SandwichsYSalados("Sandwich veggie", 1600, true))
+
+console.log (arrayDeSandwichsYSalados)
+
+
+// Seccion "Rolls y croissants" de la page Tienda de productos
+
+class RollsYCroissants {
+    constructor (nombre, precio, stock) {
+        this.nombre = nombre.toUpperCase ()
+        this.precio = parseFloat (precio)
+        this.stock = stock
+    }
+    obtenerPrecio () {
+        console.log ("El precio del producto: " + this.nombre + " es: " + this.precio + "$ ARS")
+    }
+    obtenerStock () {
+        if (this.stock === true) {
+            console.log ("Hay stock de: " + this.nombre)
+        } else {
+            console.log ("No hay stock de: " + this.nombre)
+        }
+    }
+} 
+
+const arrayDeRollsYCroissants = []
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Roll de canela", 300, true))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Roll de canela y manzana", 400, true))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Roll de canela y chocolate", 400, false))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Croissant", 350, true))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Croissant de chocolate", 450, true))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Croissant de frutos rojos", 450, false))
+
+arrayDeRollsYCroissants.push (new RollsYCroissants ("Croissant de oreo", 500, true))
+
+console.log (arrayDeRollsYCroissants)
+
+for (const prod of arrayDeRollsYCroissants) {
+    prod.obtenerStock ()
+}
+
+
+// // HACIENDO USO DE DATE:
+const diaDeHoy = new Date ()
+
+// Agregando copyright en el footer (en un futuro se verá en el html, por el momento lo consologeo)
+console.log ("Todos los derechos reservados | Le paradis | Año: " + diaDeHoy.getFullYear ())
+
+
+// // HACIENDO EL SIMULADOR DEL CARRITO:
+// IDEA: Hacer con prompt un mensaje que diga "IR A LA CATEGORIA DESEADA: CAKES, SANDWICHS O ROLLS" luego con un if, mostrar la lista de los productos de 
+// dicha seccion que escogimos, en el mismo prompt pedir que el usuario ingrese el nombre de los productos que quiere llevar y la cantidad (tener en cuenta productos en stock). Despues mostrar
+// un alert que diga el precio total del carrito. Mostrar otro prompt que pregunte si queremos saber cuantos nos sale el envio + el precio total del carrito
+// (hago uso del primer simulador interactivo). Con un if, si el usuario desea saberlo, mostrarle con alert el precio final con el envio incluido. 
