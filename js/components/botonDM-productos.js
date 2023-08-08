22d10d6dfadf75cc112525cf5542fd3419b71983
Ivan Fernandez <nahuel.nf13@gmail.com>
2023-08-08 15:22:10 -0300

    Se terminó el boton Dark Mode de la pagina productos

--- js/components/botonDM-productos.js
+++ js/components/botonDM-productos.js
@@ -20,9 +20,24 @@ const tarjetaTostadoJYQ = document.querySelector ('#tarjeta-tostadoJYQ')
 const tarjetaTostada= document.querySelector ('#tarjeta-tostada')
 const tarjetaSandwichMediterraneo = document.querySelector ('#tarjeta-sandwichMediterraneo')
 const tarjetaHamburguesaVeggie = document.querySelector ('#tarjeta-hamburguesaVeggie')
+const titulo3 = document.querySelector ('#titulo3')
+const tarjetaRollCanela = document.querySelector ('#tarjeta-rollCanela')
+const tarjetaRollManzana = document.querySelector ('#tarjeta-rollManzana')
+const tarjetaRollChocolate = document.querySelector ('#tarjeta-rollChocolate')
+const tarjetaCroissant = document.querySelector ('#tarjeta-croissant')
+const tarjetaCroissantChocolate = document.querySelector ('#tarjeta-croissantChocolate')
+const tarjetaCroissantFR = document.querySelector ('#tarjeta-croissantFR')
+const tarjetaCroissantOreo = document.querySelector ('#tarjeta-croissantOreo')
+const titulo4 = document.querySelector ('#titulo4')
+const tarjetaMedialuna = document.querySelector ('#tarjeta-medialuna')
+const tarjetaChipa = document.querySelector ('#tarjeta-chipa')
+const tarjetaScone = document.querySelector ('#tarjeta-scone')
+const tarjetaDonaChocolate = document.querySelector ('#tarjeta-donaChocolate')
+const tarjetaDonaLeParadis = document.querySelector ('#tarjeta-donaLeParadis')
 
 
 let darkMode = localStorage.getItem ("dark-mode")
+
 
 // ACTIVAR
 function activarDarkMode () {
@@ -44,7 +59,20 @@ function activarDarkMode () {
     tarjetaTostada.classList.add ("dark-mode")
     tarjetaSandwichMediterraneo.classList.add ("dark-mode")
     tarjetaHamburguesaVeggie.classList.add ("dark-mode")
-
+    titulo3.classList.add ("dark-mode")
+    tarjetaRollCanela.classList.add ("dark-mode")
+    tarjetaRollManzana.classList.add ("dark-mode")
+    tarjetaRollChocolate.classList.add ("dark-mode")
+    tarjetaCroissant.classList.add ("dark-mode")
+    tarjetaCroissantChocolate.classList.add ("dark-mode")
+    tarjetaCroissantFR.classList.add ("dark-mode")
+    tarjetaCroissantOreo.classList.add ("dark-mode")
+    titulo4.classList.add ("dark-mode")
+    tarjetaMedialuna.classList.add ("dark-mode")
+    tarjetaChipa.classList.add ("dark-mode")
+    tarjetaScone.classList.add ("dark-mode")
+    tarjetaDonaChocolate.classList.add ("dark-mode")
+    tarjetaDonaLeParadis.classList.add ("dark-mode")
 
     localStorage.setItem ("dark-mode", "activado")
     colorModeButton.innerText = "Light Mode"
@@ -71,10 +99,25 @@ function desactivarDarkMode () {
     tarjetaTostada.classList.remove ("dark-mode")
     tarjetaSandwichMediterraneo.classList.remove ("dark-mode")
     tarjetaHamburguesaVeggie.classList.remove ("dark-mode")
+    titulo3.classList.remove ("dark-mode")
+    tarjetaRollCanela.classList.remove ("dark-mode")
+    tarjetaRollManzana.classList.remove ("dark-mode")
+    tarjetaRollChocolate.classList.remove ("dark-mode")
+    tarjetaCroissant.classList.remove ("dark-mode")
+    tarjetaCroissantChocolate.classList.remove ("dark-mode")
+    tarjetaCroissantFR.classList.remove ("dark-mode")
+    tarjetaCroissantOreo.classList.remove ("dark-mode")
+    titulo4.classList.remove ("dark-mode")
+    tarjetaMedialuna.classList.remove ("dark-mode")
+    tarjetaChipa.classList.remove ("dark-mode")
+    tarjetaScone.classList.remove ("dark-mode")
+    tarjetaDonaChocolate.classList.remove ("dark-mode")
+    tarjetaDonaLeParadis.classList.remove ("dark-mode")
 
     localStorage.setItem ("dark-mode", "desactivado")
     colorModeButton.innerText = "Dark Mode"
 }
+
 
 if (darkMode === "activado") {
     activarDarkMode ()
